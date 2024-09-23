e67d292a3b545a4642bc6faeddbcd7f0a48bb18e
siriusyaoz <siriusyaozheng@mail.ustc.edu.cn>
2024-09-23 22:38:18 +0800

    syscall lab completed

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *end = sbrk(PGSIZE * 32);
+  //不能理解！，这个16是怎么来的？？
+  end = end + 16 * PGSIZE;
+  char *secret = end + 32;
+  printf("secret: %s\n", secret);
+  write(2, secret, 8);
   exit(1);
 }
